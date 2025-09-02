a06b48eadbe596df4b2c5782fa0ef16d2f05b4eb
Luffysan12 <mdafrah7@gmail.com>
2025-09-02 21:38:09 +0530

    first commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet ,Text, View } from 'react-native';
+
+
+
+export default function TabOneScreen() {
+  return (
+    <View className='flex-1 items-center justify-center bg-blue-500'>
+      <Text>Pizza Peperoni</Text>
+      <Text>$12.99</Text>
+    </View>
+  );
+}
+
