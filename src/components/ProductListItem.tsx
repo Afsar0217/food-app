148a4b98387a34eef4f5cd2e24091f9d447e33e2
Luffysan12 <mdafrah7@gmail.com>
2025-09-02 23:32:51 +0530

    Update

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,16 +1,16 @@
 import { Text, View,Image } from 'react-native';
+import { Product } from '../types';
+
+export const defaultPizzaImage='https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png';
 
 
-type ProductListItemProps = {
-  name: string;
-  image: string;
-  price: number;
-};
-
-const ProductListItem = ({ name, image, price }: ProductListItemProps) => {
+const ProductListItem = ({ name, image, price }: Product) => {
   return (
     <View className='flex flex-col justify-content-start items-center justify-center p-2 bg-white rounded-lg'>
-      <Image source={{ uri: image }} className='w-80 h-80' />
+      <Image
+        source={{ uri: image || defaultPizzaImage }}
+        className='w-80 h-80'
+      />
       <Text className='text-xl font-[600]'>{name}</Text>
       <Text className='text-light-tint my-2'>${price}</Text>
     </View>
